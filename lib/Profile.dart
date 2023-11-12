a1ab30fe5b105cff3933b817126d52a766ded5e2
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-11-13 04:05:02 +0800

    add profile card

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -7,20 +7,60 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Card(
-            child: Column(
-              children: [
-                // Image(),
-                Text('Antonius Indra Dharma Prasetya'),
-              ],
+      body: const Center(
+        child: ProfileCard(),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    double width = screenWidth * 0.5;
+    double photoHeight = screenHeight * 0.4;
+
+    return SizedBox(
+      width: width,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 4,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Photo on top
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+              child: Image.asset(
+                'profile.JPG',
+                width: width,
+                height: photoHeight,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
+            // Text at the bottom
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Antonius Indra Dharma Prasetya',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text('The Engineer you truly need')
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
